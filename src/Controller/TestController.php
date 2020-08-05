44f2eca8dbf9d6a399fe39426a6f2b8e7db7a201
JulieNobody <julie.nobody@gmail.com>
2020-08-05 15:02:12 +0200

    commit 2 test modif dans controller (l8)

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+//test pull 2
 class TestController extends AbstractController
 {
     /**
